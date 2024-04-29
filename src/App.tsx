5149622c8ad587a74dd48f5ca199bf4565664800
dotdotot@jiran.com <dotdotot@jiran.com>
2024-04-29 23:22:27 +0900

    Feat/tsconfig 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <span>zz</span>
+    <div className='flex flex-col w-full h-screen items-center justify-center'>
+      <div className='flex w-40 items-center justify-center bg-black'>
+        <span className='text-lg font-bold text-white'>zzz</span>
+      </div>
+      <div className='flex w-40 items-center justify-center bg-red-400'>
+        <span className='text-lg font-bold'>안녕하세요</span>
+      </div>
     </div>
   )
 }
