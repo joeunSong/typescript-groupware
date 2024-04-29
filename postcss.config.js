ec384a604cb934f633b9371e92e6ef66027f98c2
dotdotot@jiran.com <dotdotot@jiran.com>
2024-04-29 22:59:15 +0900

    Feat/초기 설정

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // postcss-flexbugs-fixes 플러그인 추가
+    "postcss-flexbugs-fixes": {},
+    // postcss-preset-env 플러그인 추가
+    "postcss-preset-env": {},
+  },
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div className='flex flex-col'>
+      <span>로그인 페이지 테스트</span>
+    </div>
+  )
+}
+
+export default Login
