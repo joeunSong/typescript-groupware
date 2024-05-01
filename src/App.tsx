5a59fb03c544905d90ada8aa50bf57996d5e1791
송조은 <goods@jiran.com>
2024-05-01 23:53:53 +0900

    JN-294 tailwind config 설정
    color, font 디자인 시스템 적용

--- src/App.tsx
+++ src/App.tsx
@@ -26,16 +26,19 @@ const App = (props: any) => {
   }, []);
 
   return (
-    <div className='flex flex-col w-full h-screen items-center justify-center'>
-      <div className='flex flex-col w-40 items-center justify-center bg-black'>
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
+      <div className='flex flex-col items-center justify-center w-40 bg-black'>
         <span className='text-sm font-medium text-white'>APP.tsx 테스트</span>
         <span className='text-base font-semibold text-gray-500'>tailwind 테스트</span>
         <span className='text-lg font-bold text-blue-200'>Babel 테스트</span>
       </div>
-      <div className='flex w-40 items-center justify-center bg-red-400'>
+      <div className='flex items-center justify-center w-40 bg-red-400'>
         <span className='text-lg font-bold text-indigo-300'>안녕하세요</span>
       </div>
       {/*  MUI 테스트 */}
+      <p className='text-primary font-Logo'>tailwind config test</p>
+      <p className='text-primary font-Logo'>테스트</p>
+      <p>ㅎㅇ</p>
       <Button variant='text'>Text</Button>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,59 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF7F00',
+        secondary: '#C7C7C7',
+      },
+      fontFamily: {
+        'noto-sans-kr': ['"Noto Sans KR"', 'sans-serif'],
+        'noto-sans': ['"Noto Sans"', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        '.font-default': {
+          fontFamily: ['noto-sans-kr', 'noto-sans'],
+        },
+        '.font-Logo': {
+          '@apply font-default': {},
+          fontSize: '3.125rem',
+          fontWeight: '800',
+          fontStyle: 'italic',
+        },
+        '.font-h1': {
+          '@apply font-default': {},
+          fontSize: '1.875rem',
+          weight: '600',
+        },
+        '.font-h2': {
+          '@apply font-default': {},
+          fontSize: '1.5rem',
+          weight: '600',
+        },
+        '.font-body1': {
+          '@apply font-default': {},
+          fontSize: '1.125rem',
+          weight: ' 500',
+        },
+        '.font-body1-bold': {
+          '@apply font-default': {},
+          fontSize: '1.125rem',
+          weight: '600',
+        },
+        '.font-placeholder': {
+          '@apply font-default': {},
+          fontSize: '1rem',
+          weight: '100',
+        },
+      });
+    },
+  ],
+};
