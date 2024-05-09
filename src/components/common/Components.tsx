80352b9b81df613878f8ab1ac5cee07efd2d961a
송조은 <goods@jiran.com>
2024-05-09 12:50:16 +0900

    JN-261 Feat: 계정등록 모달 UI생성

--- src/components/common/Components.tsx
+++ src/components/common/Components.tsx
@@ -1,5 +1,6 @@
-import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, makeStyles, Theme } from '@mui/material';
+import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, makeStyles, TextField, TextFieldProps, Theme } from '@mui/material';
 import { ReactNode } from 'react';
+import { Control, FieldPath, FieldValues, RegisterOptions, useController, UseControllerProps } from 'react-hook-form';
 
 interface CustomButtonProps {
   onClick?: () => void;
@@ -7,6 +8,7 @@ interface CustomButtonProps {
   variant: 'text' | 'outlined' | 'contained';
   size: 'lg' | 'md' | 'auto';
   color: 'primary' | 'secondary';
+  submit?: boolean;
 }
 
 /**
@@ -15,7 +17,7 @@ interface CustomButtonProps {
  * @param size lg: 로그인 버튼, md: 근태등록버튼
  * @returns
  */
-export const CustomButton = ({ onClick, children, variant, size, color }: CustomButtonProps) => {
+export const CustomButton = ({ onClick, children, variant, size, color, submit }: CustomButtonProps) => {
   const width: Record<CustomButtonProps['size'], string> = {
     auto: 'auto',
     md: '14.375rem',
@@ -29,6 +31,7 @@ export const CustomButton = ({ onClick, children, variant, size, color }: Custom
     <Button
       variant={variant}
       onClick={onClick}
+      type={submit ? 'submit' : 'button'}
       sx={{
         color: 'white',
         backgroundColor: colorSet[color].backgroundColor,
@@ -75,8 +78,32 @@ export const CustomModal = ({ isOpen, onClose, title, children }: CustomModalPro
         </p>
       </DialogTitle>
       <DialogContent>
-        <DialogContentText>{children}</DialogContentText>
+        <div className='pt-2'>{children}</div>
       </DialogContent>
     </Dialog>
   );
+};
+
+interface TControl<T extends FieldValues> {
+  control: Control<T>;
+  name: FieldPath<T>;
+  rules?: Omit<RegisterOptions<T>, 'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'>;
+}
+
+interface MuiProps {
+  textFieldProps?: TextFieldProps;
+}
+/**
+ * react-hook-form을 사용하기 위해 만든 customInput
+ */
+export const CustomInput = <TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>>({
+  textFieldProps, // textField를 위한 prop들, mui에서 import 해온다.
+  ...props
+}: MuiProps & UseControllerProps<TFieldValues, TName>) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(props);
+
+  return <TextField {...textFieldProps} {...field} error={!!error} helperText={!!error && error.message} />;
 };
--- src/pages/admin/Account.tsx
+++ src/pages/admin/Account.tsx
@@ -12,9 +12,14 @@ import {
   TableRow,
   TextField,
 } from '@mui/material';
-import { CustomButton, CustomModal } from '../../components/common/Components';
+import { CustomButton, CustomInput, CustomModal } from '../../components/common/Components';
 import { useState } from 'react';
-// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { Controller, useController, useForm } from 'react-hook-form';
 
 function createData(name: string, company: string, position: string, id: string, startWork: number, startAccount: number, accountStatus: string) {
   return { name, company, position, id, startWork, startAccount, accountStatus };
@@ -27,10 +32,20 @@ const rows = [
   createData('김지란4', '지란지교 소프트', '부장', 'jiran1@jiran.com', 20240330, 20240401, '재직'),
 ];
 
+interface FormValue {
+  name: string;
+  id: string;
+  password: string;
+  passwordCheck: string;
+  // rank: string;
+}
+
 /**
  * @todo api연결, 날짜 포맷
  */
 const AccountPageLayout = () => {
+  const { control, handleSubmit } = useForm<FormValue>();
+  const [value, setValue] = useState<Dayjs | null>();
   const [age, setAge] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -76,35 +91,64 @@ const AccountPageLayout = () => {
           </TableBody>
         </Table>
       </TableContainer>
+
       <CustomModal isOpen={isModalOpen} onClose={HandleCloseModal} title='계정추가'>
-        <div>
+        <form onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
           <div className='grid-box'>
             <div className='text-black font-body1'>
               이름&nbsp;<span className='text-red'>*</span>
             </div>
-            <TextField variant='outlined' />
+            <CustomInput
+              name='name'
+              control={control}
+              rules={{ required: '필수항목을 입력해주세요.', minLength: { value: 2, message: '최소 2글자 이상 입력하세요.' } }}
+              textFieldProps={{
+                variant: 'outlined',
+              }}
+            />
             <div className='text-black font-body1'>
               아이디&nbsp;<span className='text-red'>*</span>
             </div>
-            <div className='flex gap-1'>
-              <TextField variant='outlined' />
+            <div className='flex gap-2 align-center'>
+              <CustomInput
+                name='id'
+                control={control}
+                rules={{ required: '필수항목을 입력해주세요.' }}
+                textFieldProps={{
+                  variant: 'outlined',
+                }}
+              />
               <p className='font-body1'>@jiran.com</p>
             </div>
             <div className='text-black font-body1'>
               비밀번호&nbsp;<span className='text-red'>*</span>
             </div>
-            <TextField variant='outlined' />
+            <CustomInput
+              name='password'
+              control={control}
+              rules={{ required: '필수항목을 입력해주세요.' }}
+              textFieldProps={{
+                variant: 'outlined',
+              }}
+            />
             <div className='text-black font-body1'>
               비밀번호 확인&nbsp;<span className='text-red'>*</span>
             </div>
-            <TextField variant='outlined' />
+            <CustomInput
+              name='passwordCheck'
+              control={control}
+              rules={{ required: '필수항목을 입력해주세요.' }}
+              textFieldProps={{
+                variant: 'outlined',
+              }}
+            />
             <div className='text-black font-body1'>
               직위&nbsp;<span className='text-red'>*</span>
             </div>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <FormControl sx={{ minWidth: 120 }}>
               <Select value={age} onChange={handleChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
                 <MenuItem disabled value=''>
-                  <em>부서선택</em>
+                  부서선택
                 </MenuItem>
                 <MenuItem>대표</MenuItem>
                 <MenuItem>부장</MenuItem>
@@ -118,20 +162,38 @@ const AccountPageLayout = () => {
             <div className='text-black font-body1'>
               부서&nbsp;<span className='text-red'>*</span>
             </div>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <FormControl sx={{ minWidth: 120 }}>
               <Select value={age} onChange={handleChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
                 <MenuItem disabled value=''>
-                  <em>계정 상태 선택</em>
+                  계정 상태 선택
                 </MenuItem>
                 <MenuItem>개발 부서</MenuItem>
                 <MenuItem>기획 부서</MenuItem>
               </Select>
             </FormControl>
-
             <div className='text-black font-body1'>입사일</div>
-            {/* <DatePicker label="Basic date picker" /> */}
+            <LocalizationProvider dateAdapter={AdapterDayjs} dateFormats={{ monthShort: `M` }}>
+              <DemoContainer components={['DatePicker', 'DatePicker']}>
+                <DatePicker
+                  label='YYYY/MM/DD'
+                  format='YYYY-MM-DD'
+                  showDaysOutsideCurrentMonth
+                  value={value}
+                  onChange={(newValue) => setValue(newValue)}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
           </div>
-        </div>
+
+          <div className='flex justify-center gap-5 mt-6'>
+            <CustomButton variant='contained' size='auto' color='secondary'>
+              취소
+            </CustomButton>
+            <CustomButton variant='contained' size='auto' color='primary' submit>
+              저장
+            </CustomButton>
+          </div>
+        </form>
       </CustomModal>
     </div>
   );
