533944670cb471180b3ce7af519573d01349ea38
박윤환 <yh107@jiran.com>
2024-05-26 18:37:31 +0900

    JN-292 Feat: 이상근무 확인 기능 추가
    이상 근무 여부 (지각, 초과, 비정상,정상) 근무 파악
    이상 근무 여부를 달력 UI에 반영

--- src/components/user/main/MyFullCalendar.tsx
+++ src/components/user/main/MyFullCalendar.tsx
@@ -7,6 +7,7 @@ import moment from 'moment';
 import USER_API from '../../../services/user';
 import { EventContentArg } from '@fullcalendar/core';
 import { useHoliday } from '../../../hooks/useHoliday';
+import findWorkStatus from '../../../utils/findWorkStatus';
 
 interface MyFullCalendarProps {
   onWork?: boolean;
@@ -82,8 +83,23 @@ const MyFullCalendar = ({ onWork, todayWorkInfo, todayWorkInfoList }: MyFullCale
     // Check if the date is a holiday
 
     const isHoliday = holidayList.find((holiday) => moment(holiday.date).isSame(info.date, 'date'));
-    console.log();
-    return <div className={`fc-daygrid-day-number ${isHoliday ? 'holiday-date' : ''}`}>{info.dayNumberText}</div>;
+
+    const isSameDate = (day: string, day2: any) => {
+      const currentDate = moment(day2).format('YYYY-MM-DD');
+      //console.log(moment(day, 'YYYY.MM.DD').isSame(currentDate, 'day'));
+      return moment(day, 'YYYY.MM.DD').isSame(currentDate, 'day');
+    };
+
+    const workInfo = monthWorkInfo.find((_it: any) => isSameDate(_it.date, info.date));
+
+    return (
+      <div className='fc-daygrid-day-custom flex flex-1 items-center justify-between'>
+        <div className={`flex fc-daygrid-day-number ${isHoliday ? 'holiday-date' : ''}`}>{info.dayNumberText}</div>
+        {workInfo && (
+          <div className='flex w-[60px] h-[20px] rounded-[50px] bg-primary text-white items-center justify-center'>{findWorkStatus(workInfo)}</div>
+        )}
+      </div>
+    );
   }
 
   useEffect(() => {
@@ -94,7 +110,9 @@ const MyFullCalendar = ({ onWork, todayWorkInfo, todayWorkInfoList }: MyFullCale
   //console.log(holidayList);
 
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full font-noto-sans'>
+      {/* <button onClick={handlePrevMonth}>Prev</button>
+      <button onClick={handleNextMonth}>Next</button> */}
       <FullCalendar
         ref={calendarRef}
         locale='kr'
@@ -121,7 +139,7 @@ const MyFullCalendar = ({ onWork, todayWorkInfo, todayWorkInfoList }: MyFullCale
           center: 'title',
           end: 'customNext',
         }}
-        firstDay={1}
+        // firstDay={1}
         height={'80vh'}
         // aspectRatio={1.8}
 
@@ -159,6 +177,16 @@ const MyFullCalendar = ({ onWork, todayWorkInfo, todayWorkInfoList }: MyFullCale
 const renderEventContent = (eventInfo: EventContentArg) => {
   if (!eventInfo.event) return null;
   const isHoliday = eventInfo.event?.extendedProps?.isHoliday;
+  const workInfo = eventInfo.event.extendedProps?.workInfo;
+
+  const isToday = (day: string) => {
+    const currentDate = moment().format('YYYY-MM-DD');
+    return moment(day, 'YYYY.MM.DD').isSame(currentDate, 'day');
+  };
+
+  function formatTime(time: string) {
+    return moment(time).format('HH:mm');
+  }
 
   return isHoliday ? (
     <div className={`fc-event holiday-event`}>
@@ -166,11 +194,21 @@ const renderEventContent = (eventInfo: EventContentArg) => {
       {eventInfo.event?.title}
     </div>
   ) : (
-    //해당 컴포넌트 클릭 시 모달 열리게 설정
-    <div className={`fc-event work-event`} onClick={() => console.log(eventInfo.event.extendedProps?.workInfo)}>
-      <div>{eventInfo.event?.extendedProps?.workType}</div>
-      {eventInfo.event?.title}
-    </div>
+    workInfo && (
+      <>
+        {/* //해당 컴포넌트 클릭 시 모달 열리게 설정 */}
+        <div className={`fc-event work-event`} onClick={() => console.log(workInfo)}>
+          {/* <div>{eventInfo.event?.extendedProps?.workType}</div> */}
+          {/* <div className='flex justify-end'>
+            <div className='flex w-[60px] h-[20px] rounded-[50px] bg-primary text-white items-center justify-center'>{findWorkStatus(workInfo)}</div>
+          </div> */}
+
+          <div>{`출근 : ${formatTime(workInfo?.startAt)}`}</div>
+          <div>{`퇴근 : ${workInfo?.isNormal ? formatTime(workInfo?.endAt) : isToday(workInfo.date) ? '근무중' : '-'}`}</div>
+          {/* {eventInfo.event?.title} */}
+        </div>
+      </>
+    )
   );
 };
 export default MyFullCalendar;
