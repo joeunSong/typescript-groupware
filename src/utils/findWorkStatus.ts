c6fb6647fc340f0a75239741a9a1dfca5c59dca7
박윤환 <yh107@jiran.com>
2024-05-28 22:25:26 +0900

    Feat: 조퇴 항목 추가 근무 유형 중 조퇴 유형 추가

--- src/utils/findWorkStatus.ts
+++ src/utils/findWorkStatus.ts
@@ -2,13 +2,16 @@ import moment from 'moment';
 import { isToday } from './dateUtil';
 
 const findWorkStatus = (attendInfo: any) => {
-  const { isLate, isOver, isNormal, date } = attendInfo;
+  const { isLate, isOver, isNormal, date, isEarly } = attendInfo;
 
   if (isNormal === false && !isToday(date)) {
     return '비정상';
   }
   if (isLate === true) {
     return '지각';
+  }
+  if (isEarly === true) {
+    return '조퇴';
   }
   if (isOver === true) {
     return '초과';
