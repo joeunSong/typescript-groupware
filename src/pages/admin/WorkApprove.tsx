80061ba27af589d51c8961ea6a9d8741fc4b9b90
송조은 <goods@jiran.com>
2024-05-31 11:18:48 +0900

    fix: 근태조회 테이블 변경

--- src/pages/admin/WorkApprove.tsx
+++ src/pages/admin/WorkApprove.tsx
@@ -4,11 +4,22 @@ import { COMPANY_ID } from '../../constants/constant';
 import ADMIN_API from '../../services/admin';
 import DetailModal from '../../components/admin/workApprove/modal/DetailModal';
 import moment from 'moment';
+import CustomeDataTable from '../../components/common/DataTable';
+import {
+  CustomDate,
+  CustomDepartmentTitle,
+  CustomEndAt,
+  CustomStatus,
+  CustomUserName,
+  CustomWorkTypeTitle,
+} from '../../components/admin/workApprove/content/CustomColumns';
+import { CustomStartAt } from '../../components/user/approval/CustomColumns';
 
 const AdminWorkApprovePage = () => {
   const [commutes, setCommutes] = useState<any>();
   const [isCommuteDetailOpen, setIsCommuteDetailOpen] = useState(false);
   const [commuteDetailId, setCommuteDetailId] = useState<number>();
+  const [selectData, setSelectData]: any = useState(null);
 
   useEffect(() => {
     const getCommutes = async () => {
@@ -26,23 +37,92 @@ const AdminWorkApprovePage = () => {
     // console.log('commutes: ', commutes);
   });
 
-  const getDayOfWeek = (date: Date) => {
-    const week = ['일', '월', '화', '수', '목', '금', '토'];
+  // const getDayOfWeek = (date: Date) => {
+  //   const week = ['일', '월', '화', '수', '목', '금', '토'];
 
-    const dayOfWeek = week[new Date(date).getDay()];
+  //   const dayOfWeek = week[new Date(date).getDay()];
 
-    return dayOfWeek;
-  };
+  //   return dayOfWeek;
+  // };
 
   const handleAccountDetail = (commuteDetailId: number) => {
     setIsCommuteDetailOpen(true);
     setCommuteDetailId(commuteDetailId);
   };
 
+  const columns = [
+    {
+      field: 'date',
+      header: '근무일',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomDate(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+    {
+      field: 'user_name',
+      header: '이름',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomUserName(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+    {
+      field: 'department_title',
+      header: '부서',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomDepartmentTitle(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+    {
+      field: 'work_type_title',
+      header: '근무유형',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomWorkTypeTitle(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+    {
+      field: 'start_at',
+      header: '출근시간',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomStartAt(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+    {
+      field: 'end_at',
+      header: '퇴근시간',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomEndAt(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+    {
+      field: 'status',
+      header: '근태구분',
+      sortable: false,
+      body: (rowData: any, tableData: any) => CustomStatus(rowData, tableData),
+      className: 'max-w-[0px] p-0',
+      style: { width: '20%' },
+    },
+  ];
+
   return (
     <div className='flex flex-col w-full h-full gap-5 p-5 bg-white'>
       <div className='flex justify-end'></div>
-      <TableContainer component={Paper}>
+      <CustomeDataTable
+        data={commutes}
+        columns={columns}
+        headerTitle={'부서원 목록'}
+        headerTitleVisible={false}
+        selectData={selectData}
+        setSelectData={setSelectData}
+        filterVisible={false}
+        paginatorVisible={false}
+      />
+      {/* <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }}>
           <TableHead sx={{ backgroundColor: 'secondary.main' }}>
             <TableRow>
@@ -86,17 +166,13 @@ const AdminWorkApprovePage = () => {
                     </TableRow>
                   </>
                 );
-              })}
-            {isCommuteDetailOpen && commuteDetailId && (
-              <DetailModal
-                commuteDetailId={commuteDetailId}
-                isCommuteDetailOpen={isCommuteDetailOpen}
-                setIsCommuteDetailOpen={setIsCommuteDetailOpen}
-              />
-            )}
-          </TableBody>
+              })} */}
+      {isCommuteDetailOpen && commuteDetailId && (
+        <DetailModal commuteDetailId={commuteDetailId} isCommuteDetailOpen={isCommuteDetailOpen} setIsCommuteDetailOpen={setIsCommuteDetailOpen} />
+      )}
+      {/* </TableBody>
         </Table>
-      </TableContainer>
+      </TableContainer> */}
     </div>
   );
 };
